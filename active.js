a7c843ffd664a40f3c46a2df8c20033d4a4b17fd
Asif-4D <hsasif73@gmail.com>
2024-02-19 23:03:23 +0600

    next button functionality add by js

--- active.js
+++ active.js
@@ -1,3 +1,17 @@
 function next(){
-    console.log("play now");
+    const seatSection = document.getElementById('seat-area');
+    // console.log(seatSection.classList)
+    seatSection.classList.add('hidden');
+    const successMessageSection = document.getElementById('success-message');
+    // console.log(successMessageSection.classList)
+    successMessageSection.classList.remove('hidden');
+}
+function closeMsg(){
+    const successMessageSection = document.getElementById('success-message');
+    // console.log(successMessageSection.classList)
+    successMessageSection.classList.add('hidden');
+    const seatSection = document.getElementById('seat-area');
+    // console.log(seatSection.classList)
+    seatSection.classList.remove('hidden');
+    
 }
